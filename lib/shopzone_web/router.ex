bfac69abe5c5fb24bac421448655b02620a1e686
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-08-20 11:35:33 +0530

    Worked on Plug to ensure an open cart is assigned to the session.

--- lib/shopzone_web/router.ex
+++ lib/shopzone_web/router.ex
@@ -8,6 +8,7 @@ defmodule ShopzoneWeb.Router do
     plug :put_root_layout, html: {ShopzoneWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug ShopzoneWeb.Plug.SessionCart
   end
 
   pipeline :api do
